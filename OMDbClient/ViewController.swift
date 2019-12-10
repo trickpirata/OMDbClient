0c82f388796762f40147c0ee9fed24f0b351cac0
Trick Gorospe <patrick.gorospe@icloud.com>
2019-12-10 20:56:40 +0800

    Initial Commit

---
+++ OMDbClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OMDbClient
+//
+//  Created by Trick Gorospe on 12/10/19.
+//  Copyright © 2019 Trick Gorospe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
